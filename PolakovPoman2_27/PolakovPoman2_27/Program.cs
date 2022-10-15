25301256ae67daa6fc1aa5a6d8df0d2e3dcc893f
RomanPolakov <113889059+RomanPolakov@users.noreply.github.com>
2022-10-15 18:18:23 +0300

    Add files via upload

---
+++ PolakovPoman2_27/PolakovPoman2_27/Program.cs
@@ -0,0 +1,11 @@
+﻿int n, x, thirdIndex, secondIndex;
+
+Console.WriteLine("Введите n");
+n = int.Parse(Console.ReadLine());
+
+thirdIndex = n % 10;
+secondIndex = n / 10;
+
+x = thirdIndex * 100 + secondIndex;
+
+Console.WriteLine(x);
